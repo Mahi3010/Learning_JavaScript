0156aafc81dbbc35792c82a7080222d65deefaef
Mahi Gour <99043047+Mahi3010@users.noreply.github.com>
2025-04-06 08:33:06 +0000

    Done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const reg_no = 11553
+let name = "Mahi Gour"
+var city = "Sehore"
+courseName = "Computer Science"
+var city = "Rashi Gour"
+console.table([reg_no,name,city,courseName])
+console.log(reg_no)
+console.log(name)
+console.log(city)
+console.log(courseName)
