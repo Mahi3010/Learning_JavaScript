e38f9cfcbfe2ac1492f048c1cab04c6e02ae83f3
Mahi Gour <99043047+Mahi3010@users.noreply.github.com>
2025-04-13 09:57:48 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" //this treat all JS code as a newer version
+let name = "Mahi Gour" //string
+let age = 22          //number
+let islogged = true  //bollean
+let address = null  //null is a object datatype
+//null is a standalone value
+let state;         //undefined dataype
+console.table([name,age,islogged])
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof islogged)
+console.log(typeof address)
+console.log(typeof state)
+
+console.log(typeof null) //object 
+console.log(typeof undefined) //undefined
