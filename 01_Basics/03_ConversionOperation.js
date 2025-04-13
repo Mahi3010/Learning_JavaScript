ef7d6d42bf3455082884f51c22bff566f4c9af5e
Mahi Gour <99043047+Mahi3010@users.noreply.github.com>
2025-04-13 10:43:42 +0000

    conversion of datatypes

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,52 @@
+//Conversion to Number
+
+let score = "33";
+
+let valueInNumber = Number(score); //value of score is converting to Number
+
+console.log(score)
+console.log(valueInNumber)
+
+console.log(typeof (score))         //String
+console.log(typeof valueInNumber)   //Number
+
+
+let age = "22BCA"
+let ageInNumber = Number(age)
+
+console.log(age)
+console.log(ageInNumber)  //NaN
+
+console.log(typeof age)             //String
+console.log(typeof ageInNumber)    //Number
+
+
+let isHappy = true;
+let isHappyInNumber = Number(isHappy);
+console.log(isHappyInNumber)           //1
+
+let myName ="Mahi"
+let myNameInNumber = Number(myName)
+console.log(myNameInNumber)           //NaN
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+//Conversion to Boolean
+
+let isLogged = 1;
+let booleanIsLogged = Boolean(isLogged)
+console.log(booleanIsLogged)
+
+// 1 => true ; 0 => false
+// "" =>false ; "Mahi" => true
+
+
+//Conversion to String 
+let stateNo = 466001
+
+let stringStateNo = String(stateNo)
+console.log(stringStateNo)
+console.log(typeof stringStateNo) //string
