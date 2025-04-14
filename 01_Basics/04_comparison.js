67dbe3cb6779f927cd8103dc62581cf78c7cff48
Mahi Gour <99043047+Mahi3010@users.noreply.github.com>
2025-04-14 16:31:40 +0000

    compariosn

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 6)
+// console.log(2 >= 6)
+// console.log(4 < 7)
+// console.log(4 <= 7)      true because 4 is less than 7
+// console.log(3 == 6);
+// console.log(3 != 6)
+
+
+//console.log(null < 0);//false(null converted to no.,treat as 0)
+//console.log(null > 6);   //false
+// console.log(null == 0);  //false (but equality operator not convert null to 0)
+// console.log(null != 6); //true
+
+
+// comparison with undefined always give false
+// console.log(undefined == 0);
+// console.log(undefined < 6);
+// console.log(undefined <= 6);
+// console.log(undefined > 6);
+
+console.log(4 > "8");   //false(convert string into num)
+
+console.log(6 == "6");         //true ( == only check value)
+console.log(6 ==="6");  //false(it is a strict operator)also check type 
+                            
--- 01_Basics/test.js
+++
@@ -1,1 +0,0 @@
-console.log("Mahi Gour")
